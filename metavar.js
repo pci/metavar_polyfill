36f8e2df8c75e744dc9215c72495a8ac89bf47ac
Philip Ingrey <philip.ingrey@googlemail.com>
2012-05-15 11:19:42 +0100

    added matchMedia.ja (Issue #1)

--- metavar.js
+++ metavar.js
@@ -7,6 +7,35 @@
 	see http://www.w3.org/community/respimg/2012/05/13/an-alternative-proposition-to-and-srcset-with-wider-scope/#comment-754 for more info on the proposal
 */
 
+/* Includes matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
+
+window.matchMedia = window.matchMedia || window.msmatchMedia || (function(doc, undefined){
+
+  var bool,
+      docElem  = doc.documentElement,
+      refNode  = docElem.firstElementChild || docElem.firstChild,
+      // fakeBody required for <FF4 when executed in <head>
+      fakeBody = doc.createElement('body'),
+      div      = doc.createElement('div');
+
+  div.id = 'mq-test-1';
+  div.style.cssText = "position:absolute;top:-100em";
+  fakeBody.style.background = "none";
+  fakeBody.appendChild(div);
+
+  return function(q){
+
+    div.innerHTML = '&shy;<style media="'+q+'"> #mq-test-1 { width: 42px; }</style>';
+
+    docElem.insertBefore(fakeBody, refNode);
+    bool = div.offsetWidth == 42;
+    docElem.removeChild(fakeBody);
+
+    return { matches: bool, media: q };
+  };
+
+})(document);
+
 (function($){
   
 var mqs = {};
@@ -41,10 +70,12 @@ $(function(){
         }
       });
     }
-    mql.addListener(function(mq){
-      // somethings changed - check and change!
-      checkAndChange(name); 
-    });
+    if(mql.addListener){
+      mql.addListener(function(mq){
+        // somethings changed - check and change!
+        checkAndChange(name); 
+      });
+    }
     mqs[name].push({mq: mql, value: value});
   });
   for(var name in mqs){
